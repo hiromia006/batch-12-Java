74a2a523cb3b348ba93130fd274fa3a13f3f1e6f
hiromia006 <hiromia006@gmail.com>
2025-05-10 21:45:10 +0600

    first commit

---
+++ src/assignment_01/Hello.java
@@ -0,0 +1,8 @@
+package assignment_01;
+
+public class Hello {
+    // write main method with hello world
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/assignment_01/Hello_World.java
@@ -0,0 +1,19 @@
+package assignment_01;
+// a-z, A-Z, 0-9, $, _
+// 1. Class name should start with uppercase letter
+// 2. Class name should be meaningful
+// 3. Class name should not be a keyword
+// 4. Class name should not be a reserved word
+
+public class Hello_World {
+    int number=10;
+    int $age=30;
+    int height=170;
+    double amount=100.50;
+    String name="John Doe";
+    boolean isActive=true;
+    int a=500;
+     public static  final void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
